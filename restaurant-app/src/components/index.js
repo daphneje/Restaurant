7bf679e6170d370280689fc0b76c079d206068a2
chiamtj <chiamtj@gmail.com>
2022-03-03 23:44:40 +0800

    Components and Screens Updated - Add Main menu - Add folders fror components

---
+++ restaurant-app/src/components/index.js
@@ -0,0 +1,11 @@
+import CategoryBar from './CategoryBar/CategoryBar'
+import CheckoutBar from './CheckoutBar/CheckoutBar'
+import NavBar from './NavBar/NavBar'
+import PopupItem from './PopupItem/PopupItem'
+
+export {
+    CategoryBar,
+    CheckoutBar,
+    NavBar,
+    PopupItem
+};
---
+++ restaurant-app/src/screens/index.js
@@ -0,0 +1,9 @@
+import Main from "./home/Main";
+import MenuItems from "./menu/MenuItems"
+import Checkout from "./checkout/Checkout"
+
+export {
+    Main,
+    MenuItems,
+    Checkout
+};
