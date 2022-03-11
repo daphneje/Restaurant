4062347f6dfbaaa592aeeaeccdb798e30e0e8fb2
HuzaifaTP <91236840+HuzaifaTP@users.noreply.github.com>
2022-03-12 01:59:08 +0800

    Cleaned code

--- restaurant-app/src/App.js
+++ restaurant-app/src/App.js
@@ -16,7 +16,6 @@ const foodItem = { foodItemList };
 
 function App() {
   const [cartItems, setCartItems] = useState([]);
-  const [counter, setCounter] = useState(0);
 
   const addItem = (product) => {
     // console.log(cartItems)
