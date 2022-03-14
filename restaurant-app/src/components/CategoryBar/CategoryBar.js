80c868b4cce616604a4ac0dd741dd44f55c4defd
chiamtj <chiamtj@gmail.com>
2022-03-14 17:51:19 +0800

    Updated Category Bar appearance

--- restaurant-app/src/components/CategoryBar/CategoryBar.js
+++ restaurant-app/src/components/CategoryBar/CategoryBar.js
@@ -1,26 +1,23 @@
 import React from "react";
 // import {foodCategoryList} from '../../constants'
 import { Link } from "react-router-dom";
+import "./CategoryBar.css"
 
 const CategoryBar = ({foodCategory}) => {
-
-
   const {data} = foodCategory;
   console.log (foodCategory)
   console.log (data)
 
   return (
     <>
-    <ul class="overflow-auto  flex">
+    <div className="app_category_bar">
       {data.map((eachItem1) => (
-        <li class="mr-2">
-          <Link to={`/menu/${eachItem1.itemCategoryId}`}>
-             <p class="inline-block border border-blue rounded bg-blue-500 hover:border-gray-200 text-white hover:bg-blue-200 py-1 px-3 w-32">{eachItem1.title}</p> 
-          </Link>
-        </li>
+        <Link to={`/menu/${eachItem1.itemCategoryId}`}>
+            <p className="p_category_box">{eachItem1.title}</p>
+            {/* <p class="inline-block border border-blue rounded bg-blue-500 hover:border-gray-200 text-white hover:bg-blue-200 py-1 px-3 w-32">{eachItem1.title}</p> */}
+        </Link>
       ))}
-      </ul>
-
+      </div>
        
     </>
   );
